8641be32b4547e869c5b2590065e7b5db5ca91e0
Neemias Daniel <4114ne@gmail.com>
2024-06-27 07:03:53 -0300

    Primeiros arquivos UML da aula Praica_01
    Signed-off-by: Neemias Daniel <4114ne@gmail.com>

---
+++ DiagramaClasseJogo/Entity.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+class Entity:
+    def __init__(self):
+        self.Name = None
+        self.surf = None
+        self.rect = None
+        self.move = None
+
+    def move(self, ):
+        pass
+
---
+++ DiagramaClasseJogo/Level.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#-*- coding: utf-8 -*-
+
+from EntityFactory import EntityFactory
+
+class Level(EntityFactory):
+    def __init__(self):
+        self.window Surface = None
+        self.entity_list = None
+        self.name_level = None
+
+    def run(self, ):
+        pass
+
